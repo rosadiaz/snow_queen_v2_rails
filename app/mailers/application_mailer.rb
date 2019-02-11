825fb5af4bc148bb58b32a57f14dcd1a83a60cc3
Rosa Diaz Jara <rosadiazjara@gmail.com>
2019-02-10 17:16:58 -0800

    Fixes cc address to shovelsquad

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,5 +2,6 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'contact@shovelsquad.ca'
   default to: 'contact@shovelsquad.ca'
   default bcc: 'rosadiazjara@gmail.com'
+  default cc: 'contact@shovelsquad.ca'
   layout 'mailer'
 end
