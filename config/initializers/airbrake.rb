306c458285a11f4b9cea9c33412a0e56dd285f34
Rosa Diaz Jara <rosadiazjara@gmail.com>
2019-10-07 22:19:13 -0700

    Adds Airbrake keys

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -12,8 +12,8 @@ Airbrake.configure do |c|
   # project_key navigate to your project's General Settings and copy the values
   # from the right sidebar.
   # https://github.com/airbrake/airbrake-ruby#project_id--project_key
-  c.project_id = ENV['AIRBRAKE_PROJECT_ID']
-  c.project_key = ENV['AIRBRAKE_API_KEY']
+  c.project_id = ENV['214455']
+  c.project_key = ENV['f3f544b311e415819be5ebe3bdcbff17']
 
   # Configures the root directory of your project. Expects a String or a
   # Pathname, which represents the path to your project. Providing this option
